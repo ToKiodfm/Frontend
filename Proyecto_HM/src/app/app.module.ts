3396a6ac8fb115f5868cc0aed1e8fe15f4696b67
ToKiodfm <101168277+ToKiodfm@users.noreply.github.com>
2022-06-24 02:05:22 -0300

    se agregó logo argentina programa

--- Proyecto_HM/src/app/app.module.ts
+++ Proyecto_HM/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LogoAPComponent
   ],
   imports: [
     BrowserModule
