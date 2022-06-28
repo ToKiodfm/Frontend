0fc457acb4b2e391ce07d6a66ca90d8307d9abf6
ToKiodfm <101168277+ToKiodfm@users.noreply.github.com>
2022-06-28 00:55:02 -0300

    se agregó experiencia y educación

--- Proyecto_HM/src/app/app.module.ts
+++ Proyecto_HM/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { SocialesComponent } from './components/sociales/sociales.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,9 @@ import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
     LogoAPComponent,
     SocialesComponent,
     BannerComponent,
-    AcercaDeComponent
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule
