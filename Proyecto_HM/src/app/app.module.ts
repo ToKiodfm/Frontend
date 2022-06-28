4fb53fbbc5d9243b6824c1f5d7ea6fde5d316b05
ToKiodfm <101168277+ToKiodfm@users.noreply.github.com>
2022-06-27 21:35:27 -0300

    se agregaron redes sociales y borró el borrador de "bienvenidxs" y botón primary (prueba de bootstrap)

--- Proyecto_HM/src/app/app.module.ts
+++ Proyecto_HM/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { SocialesComponent } from './components/sociales/sociales.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoAPComponent
+    LogoAPComponent,
+    SocialesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule
