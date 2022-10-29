9aced79112a14ce3c0f9848d14914e39b0049e07
ToKiodfm <101168277+ToKiodfm@users.noreply.github.com>
2022-10-29 17:21:07 -0300

    backend de jwt terminado

---
+++ Proyecto_HM/BackEnd/src/main/java/com/portfolio/HM/Security/Dto/NuevoUsuario.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.HM.Security.Dto;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class NuevoUsuario {
+    private String nombre;
+    private String nombreUsuario;
+    private String Email;
+    private String Password;
+    private Set<String> roles = new HashSet<>();
+    
+    //Getter&Setters
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+
+    public Set<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<String> roles) {
+        this.roles = roles;
+    }
+    
+}
