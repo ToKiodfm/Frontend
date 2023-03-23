23f0e08fa0e7efc921db29279478df394bb7c310
ToKiodfm <101168277+ToKiodfm@users.noreply.github.com>
2023-03-22 22:13:29 -0300

    cambios en enviroment

--- Proyecto_HM/src/environments/environment.ts
+++ Proyecto_HM/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   firebase: {
     projectId: 'proyectofinal-26481',
-    appId: '1:262900521269:web:26782173c0c5a4f31e147c',
+    appId: '1:262900521269:web:c3e9129de8dbd63c1e147c',
     storageBucket: 'proyectofinal-26481.appspot.com',
     locationId: 'southamerica-east1',
     apiKey: 'AIzaSyAUslDTaYhcpahYZpe_ynyUFOdqVnj5pUc',
